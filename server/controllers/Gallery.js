0349df35d9c59184029756696ae19f8631a82c82
ConorJAT <conorjat75@gmail.com>
2024-04-21 15:27:07 -0400

    Created controller files and completed the Account controller.

---
+++ server/controllers/Gallery.js
@@ -0,0 +1,34 @@
+// Import our Gallery and Image models.
+const models = require('../models');
+const { Gallery, Image } = models;
+
+const galleryPage = async (req, res) => res.render('app');
+
+const getGallery = async (req, res) = {
+
+};
+
+const createGallery = async (req, res) = {
+
+};
+
+const removeGallery = async (req, res) = {
+
+};
+
+const addImage = async (req, res) => {
+
+};
+
+const removeImage = async (req, res) => [
+
+];
+
+module.exports = {
+    galleryPage,
+    getGallery,
+    createGallery,
+    removeGallery,
+    addImage,
+    removeImage,
+};
---
+++ server/controllers/index.js
@@ -0,0 +1,2 @@
+module.exports.Account = require('./Account.js');
+module.exports.Gallery = require('./Gallery.js');
