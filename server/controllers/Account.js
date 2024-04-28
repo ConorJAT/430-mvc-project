5747943702717dc2e6d9f56a51e7315b17f9aca4
ConorJAT <conorjat75@gmail.com>
2024-04-28 18:01:44 -0400

    Commented code in server controller files.

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,28 +3,39 @@ const models = require('../models');
 
 const { Account } = models;
 
+// loginPage() - Renders the entirety of the login page.
 const loginPage = (req, res) => { res.render('login'); };
 
+// signup() - Creates a user account and stores the data into the database.
 const signup = async (req, res) => {
+  // Retrieve account details.
   const user = `${req.body.username}`;
   const pass = `${req.body.password}`;
   const pass2 = `${req.body.password2}`;
 
+  // All fields required; if one or more missing, return with 400 error.
   if (!user || !pass || !pass2) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
 
+  // Passwords must match; if not matching, return with 400 error.
   if (pass !== pass2) {
     return res.status(400).json({ error: 'Passwords do not match.' });
   }
 
   try {
+    // Hash the account password and attempt to create new account record in database.
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username: user, password: hash });
     await newAccount.save();
+
+    // Set current session account to newly created account.
     req.session.account = Account.toAPI(newAccount);
+
+    // Redirect the user to the app.
     return res.json({ redirect: '/creator' });
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Username already in use.' });
@@ -33,65 +44,82 @@ const signup = async (req, res) => {
   }
 };
 
+// login() - Verify if the user exists, and if so, grant them entry into the app.
 const login = (req, res) => {
+  // Retrieve account details.
   const user = `${req.body.username}`;
   const pass = `${req.body.password}`;
 
+  // All fields required; if one or more missing, return with 400 error.
   if (!user || !pass) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
 
+  // Authenticate the user's username and password.
   return Account.authenticate(user, pass, async (err, account) => {
+    // If account doesn't exist or the password is invalid, return with 401 error.
     if (err || !account) {
       return res.status(401).json({ error: 'Invalid username and/or password.' });
     }
 
+    // If successful, set current session account the user and redirect to the app.
     req.session.account = Account.toAPI(account);
-
     return res.json({ redirect: '/creator' });
   });
 };
 
+// logout() - Destroys the current session and redirects user to login page.
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
 };
 
+// changePassword() - Allows the user to change their account's password.
 const changePassword = async (req, res) => {
+  // Retrieve account details.
   const oldPass = `${req.body.oldPassword}`;
   const newPass = `${req.body.newPassword}`;
   const newPass2 = `${req.body.newPassword2}`;
 
+  // All fields required; if one or more missing, return with 400 error.
   if (!oldPass || !newPass || !newPass2) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
 
+  // Old password must not match the new; if they match, return with 400 error.
   if (oldPass === newPass) {
     return res.status(400).json({ error: 'New and current passwords must not match.' });
   }
 
+  // New passwords must match; if they do not match, return with 400 error.
   if (newPass !== newPass2) {
     return res.status(400).json({ error: 'New passwords do not match.' });
   }
 
+  // Authenticate to ensure the old password really old password.
   return Account.authenticate(req.session.account.username, oldPass, async (err, account) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Invalid old password.' });
     }
 
+    // If the old password is true...
     try {
+      // Generate hash for new password and update it in the database.
       const hash = await Account.generateHash(newPass);
-
       await Account.findByIdAndUpdate(req.session.account._id, { password: hash });
+
+      // Log out password success and redirect to the app.
       console.log('Password change successful.');
       return res.json({ redirect: '/creator' });
     } catch (error) {
+      // If not successful, log the error and return 500 error.
       console.log(error);
       return res.status(500).json({ error: 'Error changing password.' });
     }
   });
 };
 
+// Export all Account controller functions to be used in the router.
 module.exports = {
   loginPage,
   signup,
--- server/controllers/Gallery.js
+++ server/controllers/Gallery.js
@@ -1,33 +1,42 @@
-// Import our Gallery and Image models.
+// Import all our models.
 const models = require('../models');
 
 const { Account, Gallery, Image } = models;
 
+// galleryPage() - Renders the entirety of the user app.
 const galleryPage = async (req, res) => { res.render('app'); };
 
+// getGalleries() - Retrieves all galleries created under the current session user.
 const getGalleries = async (req, res) => {
   console.log(req.session.account.galleryCount);
 
+  // If no galleries are created, return 200 status w/ empty array.
   if (req.session.account.galleryCount === 0) {
     return res.status(200).json({ galleries: [] });
   }
 
   try {
+    // Attempt to retrieve all galleries under current user session _id.
     const query = { owner: req.session.account._id };
     const docs = await Gallery.find(query).select('name description').lean().exec();
 
-    return res.json({ galleries: docs });
+    // If successful, return them to the user.
+    return res.status(200).json({ galleries: docs });
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     return res.status(500).json({ error: 'Error retrieving gallery data.' });
   }
 };
 
+// createGallery() - Creates a gallery object and adds it into the database.
 const createGallery = async (req, res) => {
+  // Name required; if no name provided, return 400 error.
   if (!req.body.galleryName) {
     return res.status(400).json({ error: 'Name is required to create gallery.' });
   }
 
+  // Compile gallery data into JSON object.
   const galleryData = {
     name: req.body.galleryName,
     description: req.body.galleryDescription,
@@ -35,12 +44,11 @@ const createGallery = async (req, res) => {
   };
 
   try {
+    // Attempt to create and store gallery to database.
     const newGallery = new Gallery(galleryData);
     await newGallery.save();
 
-    // req.session.gallery = Gallery.toAPI(newGallery);
-
-    // NEED TO UPDATE ACCOUNT INFO!
+    // Update gallery count for current session account.
     const doc = await Account.findOneAndUpdate(
       { _id: req.session.account._id },
       { $inc: { galleryCount: 1 } },
@@ -48,11 +56,13 @@ const createGallery = async (req, res) => {
     ).lean().exec();
     req.session.account = Account.toAPI(doc);
 
+    // Return 201 status with new gallery's name and desc.
     return res.status(201).json({
       name: newGallery.name,
       description: newGallery.description,
     });
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Gallery with entered name already exists.' });
@@ -61,20 +71,27 @@ const createGallery = async (req, res) => {
   }
 };
 
+// removeGallery() - Removes a gallery and all images associated within the
+//                   gallery from the database.
 const removeGallery = async (req, res) => {
+  // Name required; if no name provided, return 400 error.
   if (!req.body.galleryName) {
     return res.status(400).json({ error: 'Name is required to remove gallery.' });
   }
 
   try {
+    // Search for requested gallery in the database.
     const query = { owner: req.session.account._id, name: req.body.galleryName };
     const galDoc = await Gallery.findOne(query).lean().exec();
 
+    // Use found gallery's _id and remove all associated images.
     const selected = Gallery.toAPI(galDoc);
     await Image.deleteMany({ gallery: selected._id }).lean().exec();
 
+    // Remove the gallery itself.
     await Gallery.deleteOne({ _id: selected._id }).lean().exec();
 
+    // Update gallery count for current session account.
     const doc = await Account.findOneAndUpdate(
       { _id: req.session.account._id },
       { $inc: { galleryCount: -1 } },
@@ -82,52 +99,66 @@ const removeGallery = async (req, res) => {
     ).lean().exec();
     req.session.account = Account.toAPI(doc);
 
+    // Return 200 status to denote removal success.
     console.log(`Galleries remaining after removal: ${req.session.account.galleryCount}`);
-
     console.log('Gallery successfully removed.');
     return res.status(200);
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     return res.status(500).json({ error: 'An error occured removing gallery.' });
   }
 };
 
+// setGallery() - Sets the current session gallery (mainly to pull/add images from).
 const setGallery = async (req, res) => {
   try {
     console.log(req.body.name);
 
+    // Search for requested gallery in the database.
     const query = { owner: req.session.account._id, name: req.body.name };
     const doc = await Gallery.findOne(query).lean().exec();
 
+    // Set the session gallery to found gallery and return with 200 status code.
     req.session.gallery = Gallery.toAPI(doc);
     console.log(req.session.gallery);
     return res.status(200);
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     return res.status(500).json({ error: 'An error occured setting current gallery.' });
   }
 };
 
+// getImages() - Retrieves all images related to current session gallery from database.
 const getImages = async (req, res) => {
+  console.log(req.session.gallery);
+
+  // If user has not created any galleries, return status code 200 with empty array.
   if (req.session.account.galleryCount === 0) {
     return res.status(200).json({ images: [] });
   }
 
+  // If there is no current session gallery, return status code 400 with empty array.
   if (!req.session.gallery) {
     return res.status(400).json({ error: 'No galleries to retrieve images from.', images: [] });
   }
 
   try {
+    // Attempt to retrieve all images with current session gallery's _id.
     const query = { gallery: req.session.gallery._id };
     const docs = await Image.find(query).select('name info url').lean().exec();
 
-    return res.json({ images: docs });
+    // Return list of image objects with 200 status code.
+    return res.status(200).json({ images: docs });
   } catch (err) {
+    // If not successful, log the error and return 500 error.
     console.log(err);
     return res.status(500).json({ error: 'Error retrieving image data.', images: [] });
   }
 };
 
+// addImage() - Adds an image to the database.
 const addImage = async (req, res) => {
   console.log(req.session.gallery);
 
@@ -170,6 +201,7 @@ const removeImage = async (req, res) => {
   return res.status(201);
 };
 
+// Export all Gallery controller functions to be used in the router.
 module.exports = {
   galleryPage,
   getGalleries,
